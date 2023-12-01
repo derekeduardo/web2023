80a5a94fb266e141a61ef5ce0f5abedca4f03e84
derekeduardo <derekcardenas@hotmail.com>
2023-12-01 18:06:08 -0500

    view of testdrive

--- recursos/admin/api_admintestdrive.php
+++ recursos/admin/api_admintestdrive.php
@@ -6,9 +6,10 @@ require_once('../../db/configbd.php');
 
 $con = conectarDB();
 
-$sql = "SELECT testdrive.id_test, testdrive.id_carro, testdrive.id_user,t estdrive.fecha
+$sql = "SELECT testdrive.id_test, testdrive.id_carro, testdrive.id_user,testdrive.fecha,
         carros.nombre AS nombre_carro, 
-        usuarios.usuario AS usuario
+        usuarios.usuario AS usuario,
+        usuarios.correo AS correo
         FROM testdrive
         INNER JOIN carros ON testdrive.id_carro = carros.id_carro
         INNER JOIN usuarios ON testdrive.id_user = usuarios.id_usuario";
@@ -25,6 +26,7 @@ if ($query) {
                 "id_test" => $data["id_test"],
                 "id_carro" => $data["id_carro"],
                 "id_user" => $data["id_user"],
+                "correo" => $data["correo"],
                 "fecha" => $data["fecha"],
                 "nombre" => $data["nombre_carro"],
                 "usuario" => $data["usuario"]
--- views/testdrive.php
+++ views/testdrive.php
@@ -40,39 +40,43 @@
     
     ?>
 
-    <?php
+<?php
 
-    // Hacer la solicitud a la API
-    $api_url = 'http://localhost/semestral%202023/services/admin/api_admintestdrive.php';
-    $response = file_get_contents($api_url);
-    
-    if ($response !== false) {
-        $data = json_decode($response, true);
-    
-        // Manipular los datos y mostrarlos en el contenedor
-        $contenedorDatos = '<div id="datos-api">';
-        if (!empty($data)) {
-            foreach ($data as $testdrive) {
-                $contenedorDatos .= '
-                    <div>
-                        <h2>' . $testdrive['id_test'] . '</h2>
-                        <h3>' . $testdrive['id_carro'] . '</h3>
-                        <p>' . $testdrive['id_user'] . '</p>
-                        <p>' . $testdrive['fecha'] . '</p>
-                    </div>';
-            }
-        } else {
-            $contenedorDatos .= '<p>No hay registros</p>';
+// Hacer la solicitud a la API
+$api_url = 'http://localhost/semestral%202023/recursos/admin/api_admintestdrive.php';
+$response = file_get_contents($api_url);
+
+if ($response !== false) {
+    $data = json_decode($response, true);
+
+    // Manipular los datos y mostrarlos en el contenedor
+    $contenedorDatos = '<div id="datos-api">';
+
+    if (!empty($data)) {
+        foreach ($data as $testdrive) {
+            $contenedorDatos .= '
+                <div>
+                    <h2>ID Test: ' . $testdrive['id_test'] . '</h2>
+                    <h3>ID Carro: ' . $testdrive['id_carro'] . '</h3>
+                    <p>ID Usuario: ' . $testdrive['id_user'] . '</p>
+                    <p>Nombre del Carro: ' . $testdrive['nombre'] . '</p>
+                    <p>Usuario: ' . $testdrive['usuario'] . '</p>
+                    <p>Correo: ' . $testdrive['correo'] . '</p>
+                    <p>Fecha: ' . $testdrive['fecha'] . '</p>
+                </div>';
         }
-    
-        $contenedorDatos .= '</div>';
-        echo $contenedorDatos;
     } else {
-        echo 'Error al obtener datos de la API';
+        $contenedorDatos .= '<p>No hay registros</p>';
     }
 
-    
-    ?>
+    $contenedorDatos .= '</div>';
+    echo $contenedorDatos;
+} else {
+    echo 'Error al obtener datos de la API';
+}
+
+?>
+
     
 </body>
 </html>
