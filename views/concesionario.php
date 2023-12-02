083ffc8ac99e241b31e883f6c3fd04f59a217203
K-Antillon <kathleen.antillon@utp.ac.pa>
2023-12-02 00:24:06 -0500

    diseno de la interfaz de marcas

--- views/concesionario.php
+++ views/concesionario.php
@@ -9,20 +9,23 @@
     <title><?php echo $title_info ?></title>
 </head>
 <body>    
-    <nav>
-        <div>
-            <ul>
-                <li><h1>Logo</h1></li>
-                <li><div>
-                    <a href="./views/perfil.php">Perfil</a>
-                    <a href="./services/logout.php">Logout</a>
-                </div></li>
+    <!-- navbar -->
+    <nav class="navbar">
+        <div class="logo"> 
+            <h1>F&F</h1>
+        </div>
+    
+        <div class="navlink">
+            <a href="about.html">About</a>
+            <a href="servicios.html">Services</a>
+            <a href="contactos.html">Contacts</a>
+        </div>
 
-                <li><div>
-                    <a href="./views/login.php">Iniciar Sesión</a>
-                    <a href="./views/registro.php">Registrarse</a>
-                </div></li>
-            </ul>
+        <div class="autorizacion">
+            <a href="./views/login.php">Iniciar sesión</a>
+            <a href="./views/registro.php">Registrarse</a>
+            <!-- <a href="./views/perfil.php">Perfil</a>
+            <a href="./services/logout.php">Logout</a> -->
         </div>
     </nav>
 
@@ -39,12 +42,13 @@
             if (!empty($info)) {
                 foreach($info as $brand){
                     $container .= '
-                        <div style="background-color: red;">
-                            <img height="200px" src="data:image/jpg;base64,' . $brand['banner'] . '">
-                            <div>
-                                <h3>'. $brand['marca'] .'</h3>
-                                <h3>'. $brand['descripcion'] .'</h3>
+                        <div>
+                            <img class="img" src="data:image/jpg;base64,' . $brand['banner'] . '">
+                            <div class= "encabezado">
+                                <h3 class="titulo">'. $brand['marca'] .'</h3>
+                                <h3 class="parrafo">'. $brand['descripcion'] .'</h3>
                             </div>
+                            <hr class="hr">
                         </div>
                     ';
                 }
@@ -69,24 +73,29 @@
         $data = json_decode($response, true);
     
         // Manipular los datos y mostrarlos en el contenedor
-        $contenedorDatos = '<div>';
+        $contenedorDatos = '<div class="muestra">';
         if (!empty($data)) {
             foreach ($data as $carro) {
                 $contenedorDatos .= '
-                    <div>
-                        <h2>' . $carro['nombre'] . '</h2>
-                        <h3>' . $carro['marca'] . '</h3>
-                        <p>' . $carro['descripcion'] . '</p>
-                        <form action="../api.php" method="post">
-                            <input type="text" name="resource" value="favoritos" style="display:none;">
-                            <input type="text" name="service" value="add" style="display:none;">
-                            <button type="submit" name="id" value="'. $carro['id'] .'">Favorito</button>
-                        </form>
-                        <form action="../recursos/api_testdrive.php" method="post">
-                            <input type="text" name="key" value="'.$key.'" style="display: none;">
-                            <button type="submit" name="id" value="'. $carro['id'] .'">Test Drive</button>
-                        </form>
+                    <div class="carros">
                         <img height="200px" src="data:image/jpg;base64,' . $carro['imagen'] . '">
+                        <div class="separacion">
+                            <p class="modelito"> Modelo </p>
+                            <h2>' . $carro['nombre'] . '</h2>
+                            <h3 class="marca-carro">' . $carro['marca'] . '</h3>
+                        </div>
+                        <p class="carro-descripcion">' . $carro['descripcion'] . '</p>
+                        <div class="buttons">
+                            <form action="../recursos/api_testdrive.php" method="post">
+                                <input type="text" name="key" value="'.$key.'" style="display: none;">
+                                <button class="test" type="submit" name="id" value="'. $carro['id'] .'">Test Drive</button>
+                            </form>
+                            <form action="../api.php" method="post">
+                                <input type="text" name="resource" value="favoritos" style="display:none;">
+                                <input type="text" name="service" value="add" style="display:none;">
+                                <button class="fav" type="submit" name="id" value="'. $carro['id'] .'">Favorito</button>
+                            </form>
+                        </div>
                     </div>';
             }
         } else {
