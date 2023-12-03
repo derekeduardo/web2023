38d6e96139852facb9466e819cbc4c27132f502b
cristngg <officecristofer@gmail.com>
2023-12-02 22:22:53 -0500

    Vista about casi terminada

--- views/about.php
+++ views/about.php
@@ -29,10 +29,63 @@
         </div>
     </nav>  
 
-    <!-- Banner Image -->
-    <img src="../assets/Diseño sin título.png" alt="Banner Image" style="width: 100%; height: auto;">
-    <!-- Reemplaza "../assets/images/banner.jpg" con la ruta de tu imagen de banner -->
+    <div class="banner-container">
+        <img src="../assets/Diseño sin título.png" alt="Banner Image" style="width: 100%; height: auto;">
+        <div class="banner-text">
+            <h2 class="texth1">About</h2>
+            <p>Encargados de que obtengas el auto de tus sueños.</p>
+        </div>
+        <div class="button-container">
+            <button>Ver más</button>
+        </div>
+    </div>
+    </div>
+    <div class="project-section">
+        <h3>Sobre nuestro proyecto</h3>
+        <p>Bienvenido a nuestra agencia de automóviles, donde encontrar tu vehículo ideal es fácil y personalizado. Agrega tus opciones favoritas,
+        programa un test drive y gestiona tu cuenta desde cualquier lugar. Además, nuestros administradores tienen una 
+          vista completa de los autos, registros y la capacidad de mantener la información actualizada.
 
-    <!-- Resto del contenido de la página -->
+        Descubre la experiencia sin complicaciones de elegir tu próximo automóvil con nosotros. ¡Bienvenido a una forma más inteligente de comprar un coche!</p>
+    </div>
+
+   <!-- ... Tu código HTML anterior ... -->
+
+<!-- Título "Sobre nosotros" -->
+<div class="about-us-section">
+    <h2>Sobre nosotros</h2>
+    <!-- Contenedor para los párrafos de las 5 personas -->
+    <div class="persons-container">
+        <div class="person">
+            <h3>su nombre</h3>
+            <p>sobre ustedes</p>
+        </div>
+        <div class="person">
+            <h3>su nombre</h3>
+            <p>sobre ustedes 2</p>
+        </div>
+
+        <div class="person">
+            <h3>su nombre</h3>
+            <p>sobre ustedes 2</p>
+        </div>
+    </div>
+<div class="persons-container2">
+    <div class="personx">
+            <h3>su nombre</h3>
+            <p>sobre ustedes</p>
+        </div>
+
+        <div class="personx">
+            <h3>su nombre</h3>
+            <p>sobre ustedes</p>
+        </div>
+
+    <div>
+</div>
+    </div>
+</div>
+
 </body>
 </html>
+
